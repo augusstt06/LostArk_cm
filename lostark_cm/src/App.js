873d206137e0073a9a5d8627731b183182ff9be2
augusstt06 <augusstt06@gmail.com>
2022-12-08 01:40:40 +0900

    Connect Routing to Main, Detail

--- lostark_cm/src/App.js
+++ lostark_cm/src/App.js
@@ -1,11 +1,14 @@
-import Button from "react-bootstrap/Button";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./page/detail";
+import Main from "./page/main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="info">Info</Button>{" "}
-      </header>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/character:id" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
---
+++ lostark_cm/src/page/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <div className="header">
+        <h2>Dukii</h2>
+      </div>
+    </div>
+  );
+}
+export default Main;
