fcd19a460170142c7bbc317803630dd5a73aeb29
augusstt06 <augusstt06@gmail.com>
2022-12-08 21:20:42 +0900

    Resolving CORS error

--- lostark_cm/src/page/detail.js
+++ lostark_cm/src/page/detail.js
@@ -1,10 +1,40 @@
 import React from "react";
+import Header from "../component/header";
+import Input from "../component/input";
+import styles from "../css/detail.module.css";
 import { useLocation } from "react-router";
+import axios from "axios";
 
 function Detail() {
   let location = useLocation();
-  console.log(location);
-  return <div>상세</div>;
+
+  const userId = location.state.id.userId;
+  console.log(userId);
+
+  const getCharacter = async () => {
+    try {
+      const res = await axios.get(encodeURI(`/characters/${userId}/siblings`), {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+        },
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className={styles.detail}>
+      <div className={styles.detailHeader}>
+        <Header />
+        <Input />
+      </div>
+      <div className={styles.detailBody}>
+        <button onClick={getCharacter}>Test</button>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
