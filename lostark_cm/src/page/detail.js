c4957dfa376622216c0e33c43603430ff0ab4aa9
augusstt06 <augusstt06@gmail.com>
2022-12-10 19:34:57 +0900

    Render the image received through the api

--- lostark_cm/src/page/detail.js
+++ lostark_cm/src/page/detail.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../component/header";
 import Input from "../component/input";
 import Table from "../component/table";
@@ -8,11 +8,11 @@ import { customAxios } from "../api/customAxios";
 
 function Detail() {
   let location = useLocation();
-
   const userId = location.state.id.userId;
-  console.log(userId);
 
-  const getCharacter_Armor = async () => {
+  const [info, setInfo] = useState();
+
+  const getCharacter_Profile = async () => {
     try {
       const res = await customAxios.get(
         encodeURI(`/armories/characters/${userId}/profiles`)
@@ -22,7 +22,22 @@ function Detail() {
       console.log(err);
     }
   };
+  const getCharacter_Armor = async () => {
+    try {
+      const res = await customAxios.get(
+        encodeURI(`/armories/characters/${userId}/equipment`)
+      );
+      console.log(res.data);
+      setInfo(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(info, "info");
+  const test = info[0]["Icon"];
+  console.log(test);
   useEffect(() => {
+    getCharacter_Profile();
     getCharacter_Armor();
   }, [userId]);
 
@@ -33,19 +48,30 @@ function Detail() {
         <Input />
       </div>
       <div className={styles.detailBody}>
-        <div class="btn-group" role="group" aria-label="Basic outlined example">
-          <button type="button" class="btn btn-outline-primary">
+        <div
+          className="btn-group"
+          role="group"
+          aria-label="Basic outlined example"
+        >
+          <button type="button" className="btn btn-outline-primary">
             발탄
           </button>
-          <button type="button" class="btn btn-outline-primary">
+          <button type="button" className="btn btn-outline-primary">
             비아키스
           </button>
-          <button type="button" class="btn btn-outline-primary">
+          <button type="button" className="btn btn-outline-primary">
             쿠크세이튼
           </button>
-          <button type="button" class="btn btn-outline-primary">
+          <button type="button" className="btn btn-outline-primary">
             아브렐슈드
           </button>
+        </div>
+        <div>
+          <div>
+            <div>
+              <img src={test} />
+            </div>
+          </div>
         </div>
         {/* <Table /> */}
       </div>
