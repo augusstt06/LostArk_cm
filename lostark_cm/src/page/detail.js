02c2485152484fbb6560a0827326e362148c4d8a
augusstt06 <augusstt06@gmail.com>
2022-12-11 14:17:17 +0900

    Modify Detail UI

--- lostark_cm/src/page/detail.js
+++ lostark_cm/src/page/detail.js
@@ -15,7 +15,8 @@ function Detail() {
   let location = useLocation();
   const userId = location.state.id.userId;
 
-  const [info, setInfo] = useState();
+  const [armor, setArmor] = useState();
+  const [engrave, setEngrave] = useState();
   const [raid, setRaid] = useState("");
 
   const selectRaid = (e) => {
@@ -23,11 +24,12 @@ function Detail() {
   };
   useEffect(() => {
     getCharacter_Profile(userId);
-    getCharacter_Armor(userId, setInfo);
-    getCharacter_Engrave(userId);
+    getCharacter_Armor(userId, setArmor);
+    getCharacter_Engrave(userId, setEngrave);
   }, [userId]);
-  console.log(raid);
 
+  console.log(engrave);
+  console.log(armor);
   return (
     <div className={styles.detail}>
       <div className={styles.detailHeader}>
@@ -40,36 +42,36 @@ function Detail() {
         </div>
         <div className={styles.spec}>
           <div className={styles.user}>
-            {info &&
-              info.map((data) =>
-                data["Type"] === "무기" ||
-                data["Type"] === "투구" ||
-                data["Type"] === "상의" ||
-                data["Type"] === "하의" ||
-                data["Type"] === "장갑" ||
-                data["Type"] === "견갑" ? (
-                  <div key={data["Name"]}>
-                    <img src={data["Icon"]} alt={data["Name"]} />
-                    {data["Name"]}
-                  </div>
-                ) : null
-              )}
+            <p>장비</p>
+            <div className={styles.user_armor}>
+              {armor &&
+                armor.map((data) =>
+                  data["Type"] === "무기" ||
+                  data["Type"] === "투구" ||
+                  data["Type"] === "상의" ||
+                  data["Type"] === "하의" ||
+                  data["Type"] === "장갑" ||
+                  data["Type"] === "견갑" ? (
+                    <div key={data["Name"]}>
+                      <img src={data["Icon"]} alt={data["Name"]} />
+                      <div className={styles.armor_tooltip}>
+                        <div>{data["Type"]}</div>
+                        <div>{data["Name"]}</div>
+                      </div>
+                    </div>
+                  ) : null
+                )}
+            </div>
+            <p>각인 효과</p>
+            {engrave &&
+              engrave.map((data) => (
+                <>
+                  <div>{data["Name"]}</div>
+                </>
+              ))}
           </div>
           <div className={styles.control_group}>
-            {info &&
-              info.map((data) =>
-                data["Type"] === "무기" ||
-                data["Type"] === "투구" ||
-                data["Type"] === "상의" ||
-                data["Type"] === "하의" ||
-                data["Type"] === "장갑" ||
-                data["Type"] === "견갑" ? (
-                  <div key={data["Name"]}>
-                    <img src={data["Icon"]} alt={data["Name"]} />
-                    {data["Name"]}
-                  </div>
-                ) : null
-              )}
+            <div>각인 효과</div>
           </div>
           <div>여기는 경매장 가격</div>
         </div>
